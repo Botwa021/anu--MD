9fa6140061aa87042544802e60ab9d0df4e10f5c
Mursid S <97183746+Botwa021@users.noreply.github.com>
2022-06-04 21:12:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,43 +1,45 @@
+//wa.me/6288233832771
+//edit sendiri aja okey
 //=============『 Utama 』================== //
-global.owner = ['6287892711054'] 
-global.mods = [] 
+global.owner = ['6288233832771'] 
+global.mods = ['6288233832771'] 
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Hyzer Official'
-global.numberowner = '6287892711054'
-global.instagram = 'https://instagram.com/hyzer_stfu'
-global.github = 'https://github.com/Hyzerr'
-global.dana = '087892711054'
-global.pulsa = '085212023766'
-global.gopay = '087892711054'
+global.nameowner = '𝘔𝘶𝘳𝘴𝘪𝘥 𝘚'
+global.numberowner = '6288233832771'
+global.instagram = 'https://instagram.com/mursid.st'
+global.github = 'https://github.com/Botwa021'
+global.dana = '088233832771'
+global.pulsa = '089612698583'
+global.gopay = '088233832771'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'ShiroBot-Md'
-global.gc = 'https://chat.whatsapp.com/Jzd9DEVB5nODtNBk1VCNrV'
-global.web = 'https://hyzerr-apii.herokuapp.com' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
-global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
+global.namebot = '𝘽𝙊𝙏𝘾𝘼𝙃𝙓'
+global.gc = 'https://chat.whatsapp.com/LPFQ2X1cnihB0fb8F8cZau'
+global.web = 'https://instagram.com/mursid.st' 
+global.price1 = '1 bulan = 20.000\n2 Minggu 10.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
+global.fotonya1 = 'https://telegra.ph/file/7cb142e58fcb16f6d6af9.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/7cb142e58fcb16f6d6af9.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '𝚂𝙷𝙸𝚁𝙾-𝙼𝙳'
+global.wm = '© 𝘽𝙊𝙏𝘾𝘼𝙃𝙓'
 global.watermark = wm
 global.wm2 = '                     「 𝚂𝙷𝙸𝚁𝙾-𝙼𝙳 あ⁩ 」'
-global.wm3 = '⫹⫺ 𝚂𝙷𝙸𝚁𝙾-𝙼𝙳'
-global.htki = '––––––『' 
-global.htka = '』––––––'
-global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
+global.wm3 = '© 𝘽𝙊𝙏𝘾𝘼𝙃𝙓'
+global.htki = '『' 
+global.htka = '』'
+global.media = 'https://telegra.ph/file/32e717f19e0ea6aadd0a4.jpg'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wait = '_*tunggu sedang di proses...*_'
+global.wait = '_*tunggu...*_'
 global.eror = '_*Server Error*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
-global.stiker_wait = 'Stiker sedang dibuat'
+global.stiker_wait = '_*tunggu...*_'
 global.packname = '2022'
-global.author = '© Hyzer Official'
+global.author = '© 𝘔𝘶𝘳𝘴𝘪𝘥 𝘚'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
