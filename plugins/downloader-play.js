ac695f9f67f285c1dc943e27ac4048df65c3d56b
Mursid S <97183746+Botwa021@users.noreply.github.com>
2022-06-05 01:31:33 +0700

    Update downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -28,13 +28,14 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
   if (yt2 === false) throw 'semua server gagal'
   let { dl_link, thumb, title, filesize, filesizeF } = yt
 let anu =  `
-*Judul:* ${title}
-*Ukuran File Audio:* ${filesizeF}
-*Ukuran File Video:* ${yt2.filesizeF}
-*Server y2mate:* ${usedServer}
-*Link Sumber:* 
-${vid.url}
-
+┏────『 _*PLAY YOUTUBE*_ 』───⬣
+│⬡ *Judul* : ${title}
+│⬡ *Ukuran File Audio* : ${filesizeF}
+│⬡ *Ukuran File Video* : ${yt2.filesizeF}
+│⬡ *Server y2mate* : ${usedServer}
+│⬡ *Link Sumber* : 
+│⬡ ${vid.url}
+┗─────────────⬣
 `
      let message = await prepareWAMessageMedia({ image: await (await require('node-fetch')(thumb)).buffer()}, { upload: conn.waUploadToServer }) 
       const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
