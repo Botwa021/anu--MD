1fd86ba72d51a34525f4aecf79646179aec7c1c2
Mursid S <97183746+Botwa021@users.noreply.github.com>
2022-06-08 21:25:09 +0700

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -290,7 +290,6 @@ let menuu = `┏────『 *${namebot}* 』───⬣
 │⬡ *Baterai* : ${conn.battery != undefined ? `${conn.battery.value}% ${conn.battery.live ? '🔌 Charger' : ''}` : 'tidak diketahui'}
 │⬡ *Pengguna* : ${Object.keys(global.db.data.users).length}
 │⬡ *Mode* : ${global.opts['self'] ? 'Self' : 'publik'}
-│⬡ *Terblock* : ${conn.blocklist.length} 
 │⬡ *Chat Terbanned* : ${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length} 
 │⬡ *Pengguna Terbanned* : ${Object.entries(global.db.data.users).filter(user => user[1].banned).length} 
 │⬡ *Api* : ${tag}
